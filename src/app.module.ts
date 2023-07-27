e7f071f8c5258548b7b12ff326cfe4c518d28c5b
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-07-27 11:04:06 +0330

    fix: add app pipe to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { AuthModule } from '@auth/auth.module';
 import { ConfigModule } from '@config/config.module';
 import { ModelsModule } from '@models/models.module';
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { ProvidersModule } from '@providers/providers.module';
 
 @Module({
   imports: [AuthModule, ConfigModule, ModelsModule, ProvidersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidUnknownValues: true,
+        forbidNonWhitelisted: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
